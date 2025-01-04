9f12f82ac38c06f3d82154fa6e3888fcaaf79fe2
TheS1lentArr0w <paulocruz0123@hotmail.com>
2025-01-04 01:10:12 +0000

    feat: updated fugitive with more keymaps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,7 +1,29 @@
 return {
 	"tpope/vim-fugitive",
 
-	config = function() 
+	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+        local Paulo_Fugitive = vim.api.nvim_create_augroup("Paulo_Fugitive", {})
+
+        local autocmd = vim.api.nvim_create_autocmd
+        autocmd("BufWinEnter", {
+            group = Paulo_Fugitive,
+            pattern = "*",
+            callback = function()
+                if vim.bo.ft ~= "fugitive" then
+                    return
+                end
+
+                local bufnr = vim.api.nvim_get_current_buf()
+                local opts = {buffer = bufnr, remap = false}
+                vim.keymap.set("n", "<leader>p", function()
+                    vim.cmd.Git('push')
+                end, opts)
+            end,
+        })
+
+        vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>")
+        vim.keymap.set("n", "gj", "<cmd>diffget //3<CR>")
 	end
 }
