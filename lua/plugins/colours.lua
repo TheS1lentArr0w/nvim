ca6ccb118c475a34a54c20547facf6b0fa80c3a8
Paulo Cruz <paulo.cruz@icabbi.com>
2025-05-21 11:15:19 +0100

    formatting

--- lua/plugins/colours.lua
+++ lua/plugins/colours.lua
@@ -6,8 +6,8 @@ function ColorMyPencils(color)
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-return { 
-	"rose-pine/neovim", 
+return {
+	"rose-pine/neovim",
 	name = "rose-pine",
 	config = function()
 		require('rose-pine').setup({
